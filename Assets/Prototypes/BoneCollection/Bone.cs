ab60f5800dd6c865c21eb1105851ecf2cefabaf4
jordantkay21 <jordantkay21@gmail.com>
2024-08-04 22:15:20 -0400

    Updated.BoneCollection
    Removed Timer & Score logic from BoneCollection as it was out of scope of that feature.
    
    Also pulled collection logic out of player prefab and placed into a new script "Bone"

---
+++ Assets/Prototypes/BoneCollection/Bone.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Bone : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
